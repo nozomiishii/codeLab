dbea98920cf03de1920c283386555a3b23f0d254
Nozomi Ishii <non.ankt@gmail.com>
2019-11-17 04:22:33 +0900

    creating timer

--- timer/app.js
+++ timer/app.js
@@ -1,1 +1,17 @@
 console.log('!!!');
+
+let timer = document.querySelector('.timer');
+let time = 3;
+
+const setTimer = setInterval(function(){
+  timer.textContent = time;
+  console.log(time);
+  time--
+  if(time < 0){
+    console.log('finish!')
+    clearInterval(setTimer);
+  }
+}, 1000);
+
+
+
