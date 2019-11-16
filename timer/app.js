c0d3a3555746b83c471f077a46af07bfb626153c
Nozomi Ishii <non.ankt@gmail.com>
2019-11-17 04:55:16 +0900

    creating timer

--- timer/app.js
+++ timer/app.js
@@ -2,16 +2,18 @@ console.log('!!!');
 
 let timer = document.querySelector('.timer');
 let time = 3;
+const btn = document.querySelector('.btn');
 
-const setTimer = setInterval(function(){
-  timer.textContent = time;
-  console.log(time);
-  time--
-  if(time < 0){
-    console.log('finish!')
-    clearInterval(setTimer);
-  }
-}, 1000);
-
-
+btn.addEventListener('click',function(){
+    btn.classList.add('hide');
+    const setTimer = setInterval(function(){
+      timer.textContent = time;
+      console.log(time);
+      time--
+      if(time < 0){
+        console.log('finish!')
+        clearInterval(setTimer);
+      }
+    }, 1000);
+});
 
