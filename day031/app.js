a05c759f4995d1fa1d99e3f5930bf96de8d80d47
Nozomi Ishii <non.ankt@gmail.com>
2019-11-18 13:45:06 +0900

    creating timer

---
+++ day031/app.js
@@ -0,0 +1,34 @@
+console.log('!!!');
+
+let timer = document.querySelector('.timer');
+let time = 3;
+const start = document.querySelector('.start');
+const clear = document.querySelector('.clear');
+const choices = document.querySelectorAll('.choice');
+
+for(choice of choices){
+  choice.addEventListener('click',function(){
+    time = this.dataset.time;
+    timer.textContent = `⏰ ${time} ⏰`;
+    console.log(time);
+
+  });
+}
+
+
+start.addEventListener('click',function(){
+    // btn.classList.add('hide');
+  const setTimer = setInterval(function(){
+    timer.textContent = time;
+    console.log(time);
+    time--
+    if(time < 0){
+      console.log('finish!')
+      clearInterval(setTimer);
+    }
+  }, 1000);
+});
+
+clear.addEventListener('click',function(){
+  time = 0;
+});
