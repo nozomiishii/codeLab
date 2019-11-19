5a83bfe097787a3242c9021e3cfaf20e4d1b6614
Nozomi Ishii <non.ankt@gmail.com>
2019-11-19 20:48:33 +0900

    test

--- day033/app.js
+++ day033/app.js
@@ -1,1 +1,7 @@
-console.log('!!!');
+const boxes = document.querySelectorAll('.box');
+const items = ["🗡", "🛡", "💰", "💩"];
+for(box of boxes){
+  box.addEventListener('click',function(){
+    console.log(this)
+  });
+}
---
+++ ememy/app.js
@@ -0,0 +1,11 @@
+//check
+console.log('js is here');
+//game start
+const start = document.getElementById('start');
+const op = document.querySelector('.op');
+
+start.addEventListener('click',function(){
+  op.classList.add('start');
+});
+
+// write game code from here
