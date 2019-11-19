4d851141be3c00a59c3bdd81bec3b67d64974c8b
Nozomi Ishii <non.ankt@gmail.com>
2019-11-20 03:43:52 +0900

    treasure3

--- day033/app.js
+++ day033/app.js
@@ -1,7 +1,22 @@
 const boxes = document.querySelectorAll('.box');
 const items = ["🗡", "🛡", "💰", "💩"];
+const goodH2 = document.querySelector('.good h2');
+const good = document.querySelector('.good');
+
 for(box of boxes){
   box.addEventListener('click',function(){
-    console.log(this)
+    let random = Math.floor(Math.random()*items.length);
+    const item = items[random];
+    goodH2.textContent = item;
+    setTimeout(function(){
+       good.classList.add('ended');
+    },500);
   });
 }
+
+// hidden event
+const hidden = document.querySelector('.hidden');
+const bad = document.querySelector('.bad');
+hidden.addEventListener('click',function(){
+  bad.classList.add('ended');
+});
