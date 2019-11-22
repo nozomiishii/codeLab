0adaefb40ab6e14e7a04fb30a3f05a1427ccc468
Nozomi Ishii <non.ankt@gmail.com>
2019-11-22 22:47:32 +0900

    genie came out sucessfully

--- day036/app.js
+++ day036/app.js
@@ -10,16 +10,18 @@ start.addEventListener('click',function(){
 
 // write game code from here
 const you = document.querySelector('.you');
-const btn = document.querySelector('.btn');
+const ep1 = document.querySelector('.ep1');
 const wander = document.querySelector('.wander');
 const battle = document.querySelector('.battle');
 const comment = document.querySelector('.comment p');
 
-btn.addEventListener('click', function(){
-  btn.style.visibility = 'hidden';
+ep1.addEventListener('click', function(){
+  ep1.style.visibility = 'hidden';
   you.classList.add('walk');
   wander.classList.add('flash');
   setTimeout(function(){
+    // vase come out
+    wander.style.background ="#111111"
     battle.classList.add('appare');
   },4000);
   setTimeout(function(){
@@ -30,10 +32,60 @@ btn.addEventListener('click', function(){
 
 // genie came out!!!
 
+// genie status
+const monster = {
+  name: 'genie',
+  attack: 999,
+  defense: 999,
+  hp: 999,
+  comment(){
+    console.log(`I'm ${this.name}. Attack:${this.attack} Defense: ${this.defense} HP:${this.hp}`);
+  }
+};
+
+monster.comment();
 
 const vase = document.querySelector('.vase');
 const genie = document.querySelector('.genie');
 
 vase.addEventListener('click', function(){
   genie.classList.add('rub');
+  comment.textContent = `I'm ${monster.name}. Attack:${monster.attack} Defense: ${monster.defense} HP:${monster.hp}`
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
