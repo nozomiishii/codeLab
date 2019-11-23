c5d48644906c56eae79d18ff49c6cdeb97fe7796
Nozomi Ishii <non.ankt@gmail.com>
2019-11-24 05:32:21 +0900

    creating enemy roulette

--- ememy/app.js
+++ ememy/app.js
@@ -9,3 +9,11 @@ start.addEventListener('click',function(){
 });
 
 // write game code from here
+const startBtn = document.querySelector('.startBtn')
+const stopBtn = document.querySelector('.stopbtn')
+const comment = document.querySelector('.comment')
+// const commentH1 = document.querySelector('.comment h1')
+
+startBtn.addEventListener('click',function(){
+  comment.classList.add('enemy');
+});
