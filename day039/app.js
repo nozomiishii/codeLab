42f2312cb09bf511e0a65afd434224271fc75f98
Nozomi Ishii <non.ankt@gmail.com>
2019-11-25 04:56:32 +0900

    studying about class

--- day039/app.js
+++ day039/app.js
@@ -16,14 +16,32 @@ const commentP = document.querySelector('.comment p');
 const enemiesBox = document.querySelector('.enemies-box');
 
 
-const enemies = ["🦸🏻‍♂️", "🧝🏻‍♀️", "🧞‍♂️", "🧖‍♂️", "🧙‍♀️"]
+// const enemies = ["🦸🏻‍♂️", "🧝🏻‍♀️", "🧞‍♂️", "🧖‍♂️", "🧙‍♀️"]
+
+// for(enemy of enemies){
+//   const gridItem = document.createElement('h1');
+//   const gridContent = document.createTextNode(enemy);
+//   gridItem.appendChild(gridContent);
+//   enemiesBox.appendChild(gridItem);
+// }
 
 
-
-
-for(enemy of enemies){
-  const gridItem = document.createElement('h1');
-  const gridContent = document.createTextNode(enemy);
-  gridItem.appendChild(gridContent);
-  enemiesBox.appendChild(gridItem);
+class Enemy{
+  constructor(emoji, name, attack, defense, hp){
+    this.emoji = emoji,
+    this.name = name,
+    this.attack = attack,
+    this.defense = defense,
+    this.hp = hp
+  }
 }
+
+const hero = new Enemy("🦸🏻‍♂️", "Hero", 100, 80, 30);
+const elf = new Enemy("🧝🏻‍♀️", "elf", 80, 100, 30);
+const genie = new Enemy("🧞‍♂️", "genie", 100, 100, 10);
+const man = new Enemy("🧖‍♂️", "man", 50, 50, 100);
+const wizard = new Enemy("🧙‍♀️", "wizard", 80, 80, 80);
+
+const enemies = [hero, elf, genie, man, wizard];
+
+console.log(enemies[0].emoji);
