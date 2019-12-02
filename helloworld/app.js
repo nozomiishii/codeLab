98e220c582daea87d33472e4a51977f7e376c2d0
Nozomi Ishii <non.ankt@gmail.com>
2019-12-03 05:06:25 +0900

    day45

--- helloworld/app.js
+++ helloworld/app.js
@@ -11,10 +11,29 @@ start.addEventListener('click',function(){
 // write game code from here
 
 const color = document.querySelector('.comment h1');
+const btn = document.querySelector('.btn');
 
-let colorValue = 0;
-setInterval(function(){
+
+
+//これを
+// let colorValue = 0;
+// let clear = colorValue;
+//こう書けるっぽい
+let colorValue = 0, clear;
+
+btn.addEventListener('click',function(){
+  if(btn.classList.contains('started')){
+    clearInterval(clear);
+    btn.classList.remove('started');
+    console.log('stop');
+  } else {
+    clear = setInterval(changeColor, 100);
+    btn.classList.add('started');
+  }
+});
+
+function changeColor(){
+  console.log('start');
   colorValue++
   color.style.background = `linear-gradient(to left, rgb(${colorValue}, 100, 100), rgb(200,200,200))`;
-
-}, 100);
+}
