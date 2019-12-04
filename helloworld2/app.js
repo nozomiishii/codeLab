24aae07c3101d4f89fbc77e83eb22233074eadb9
Nozomi Ishii <non.ankt@gmail.com>
2019-12-05 03:53:33 +0900

    botsu

--- helloworld2/app.js
+++ helloworld2/app.js
@@ -12,31 +12,40 @@ start.addEventListener('click',function(){
 
 const color = document.querySelector('.comment h1');
 const btn = document.querySelector('.btn');
+const red = document.querySelector("#red");
+const green = document.querySelector("#green");
+const blue = document.querySelector("#blue");
 
-
-
+red.addEventListener('change',function(){
+  color.style.background = `linear-gradient(to left, rgb(${red.value}, ${green.value}, ${blue.value}), rgb(${255 - red.value}, ${255 - green.value}, ${255 - blue.value})`;
+  console.log(red.value);
+});
+green.addEventListener('change',function(){
+  color.style.background = `linear-gradient(to left, rgb(${red.value}, ${green.value}, ${blue.value}), rgb(${255 - red.value}, ${255 - green.value}, ${255 - blue.value})`;
+  console.log(green.value);
+});
+blue.addEventListener('change',function(){
+  color.style.background = `linear-gradient(to left, rgb(${red.value}, ${green.value}, ${blue.value}), rgb(${255 - red.value}, ${255 - green.value}, ${255 - blue.value})`;
+  console.log(blue.value);
+});
 //これを
 // let colorValue = 0;
 // let clear = colorValue;
 //こう書けるっぽい
-let colorValue = 0, clear;
+let redValue = red.value, redClear;
+let greenValue = green.value, greenClear;
+let blueValue = blue.value, blueClear;
+
 
 btn.addEventListener('click',function(){
-  if(btn.classList.contains('started')){
-    clearInterval(clear);
-    btn.classList.remove('started');
-    console.log('stop');
-  } else {
-    clear = setInterval(changeColor, 100);
-    btn.classList.add('started');
+  if(color.classList.contains('paused')){
+    color.classList.remove('paused')
+    color.style.animationPlayState = "running";
+  } else{
+    color.classList.add('paused')
+    color.style.animationPlayState = "paused";
   }
 });
-
-function changeColor(){
-  console.log('start');
-  colorValue ++;
-  color.style.background = `linear-gradient(to left, rgb(${colorValue}, 0, 0), rgb(0,255,255))`;
-}
 
 
 
