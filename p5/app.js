6f4dcab8ed1686f49277f46f31aa27dd5c40ed71
Nozomi Ishii <non.ankt@gmail.com>
2019-12-07 03:54:39 +0900

    p5 boilerplate

---
+++ p5/app.js
@@ -0,0 +1,11 @@
+//check
+console.log('js is here');
+//game starts
+const start = document.getElementById('start');
+const op = document.querySelector('.op');
+
+start.addEventListener('click',function(){
+  op.classList.add('start');
+});
+
+// write game code from here
