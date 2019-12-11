27bdf3cbb5c84dea45390c3a1830b7f54a9f9005
Nozomi Ishii <non.ankt@gmail.com>
2019-12-11 20:15:23 +0900

    nobanashi

--- nobanashi2/sketch.js
+++ nobanashi2/sketch.js
@@ -3,12 +3,22 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 class Bone {
-  constructor(x, y, size){
+  constructor(x, y, speedX, speedY, size){
     this.x = x;
     this.y = y;
+    this.speedX = speedX;
+    this.speedY = speedY;
   }
   show(){
     text('🦴', this.x, this.y)
+  }
+  move(){
+    if(this.x > width || this.x <= 0 || this.y > height || this.y <= 0){
+      this.speedX = this.speedX * -1;
+      this.speedY = this.speedY * -1;
+    }
+    this.x += this.speedX;
+    this.y += this.speedY;
   }
 }
 
@@ -27,7 +37,12 @@ let bone;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   dog = new Dog();
-  bone = new Bone(random(width), random(height));
+  bone = new Bone(
+    random(width),
+    random(height),
+    random(-1,1),
+    random(-1,1)
+    );
 }
 
 let counter = 0;
@@ -36,10 +51,13 @@ function draw(){
   textSize(50);
   dog.show();
   bone.show();
+  bone.move();
   let d = dist(mouseX, mouseY, bone.x, bone.y)
   if (d < 20){
     bone.x = random(width);
     bone.y = random(height);
+    bone.speedX = random(-1,1);
+    bone.speedY = random(-1,1);
     counter++
   }
   textAlign(CENTER);
