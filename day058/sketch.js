f51b4ca4b4a4304615c8d2f80bd3515cfb03f1c3
Nozomi Ishii <non.ankt@gmail.com>
2019-12-14 03:39:14 +0900

    day58

--- day058/sketch.js
+++ day058/sketch.js
@@ -1,53 +1,14 @@
 console.log('ready to sketch');
-
-class Ball{
-  constructor(x, y, r, colorR, colorG, colorB, alpha, speedX, speedY, times){
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.colorR = colorR;
-    this.colorG = colorG;
-    this.colorB = colorB;
-    this.alpha = alpha;
-    this.speedX = speedX;
-    this.speedY = speedY;
-    this.times = times;
-  }
-  show(){
-    stroke(random(255),random(255),random(255));
-    fill(this.colorR, this.colorG, this.colorB);
-    ellipse(this.x, this.y, this.r * 2);
-  }
-  move(){
-    if(this.x > width || this.y > height || this.x < 0 || this.y < 0){
-      this.speedX = this.speedX * -1
-      this.speedY = this.speedY * -1
-    }
-    this.x = this.x + this.speedX;
-    this.y = this.y + this.speedY;
-  }
-}
-
-class Beam {
-  constructor(x, y, speed){
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-  }
-  shoot(){
-    stroke(6);
-    fill(random(255));
-    ellipse(this.x, this.y, 5, 20);
-    this.y += this.speed;
-  }
-}
-
-
 let balls = [];
 let ball;
 let player;
 let beams = [];
 let beam;
+let backgroundColor = 236;
+let backgroundColorRate = 50;
+
+// let exploded = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ball = new Ball(
@@ -100,7 +61,12 @@ function draw(){
       text(this.image, this.x, this.y);
     }
   }
-  background(238);
+  // if(exploded){
+  //   console.log('!!');
+  //   backgroundColor();
+  // } else {
+    background(backgroundColor);
+  // }
   player.show();
   for(ball of balls){
     ball.show();
@@ -108,18 +74,25 @@ function draw(){
   }
   for(beam of beams){
     beam.shoot();
-    for( ball of balls){
+    for([index, ball] of balls.entries()){
       let d = dist(ball.x, ball.y, beam.x, beam.y );
       if(d < 50){
         ball.colorR = random(255);
         ball.colorG = random(255);
         ball.colorB = random(255);
+        ball.alpha = random(255);
+        ball.speedX += random(-1,1);
+        ball.speedY += random(-1,1);
         ball.r++
         ball.times++
         console.log(ball.times);
       }
-      if(ball.times > 50){
-        // erase ball==================================
+      if(ball.times > 70){
+        balls.splice(index, 1);
+        if(backgroundColor <= 0 || backgroundColor >= 255){
+          backgroundColorRate = backgroundColorRate * -1;
+        }
+          backgroundColor -= backgroundColorRate;
       }
     }
   }
