8430e52c491793791cfebc6b48f5c07f973dc52f
Nozomi Ishii <non.ankt@gmail.com>
2019-12-15 22:04:15 +0900

    zombie

--- word2/class.js
+++ word2/class.js
@@ -1,1 +1,13 @@
 console.log("class.js");
+
+class Beam {
+  constructor(icon, x, y,){
+    this.icon = icon;
+    this.x = x;
+    this.y = y;
+  }
+  shoot(){
+    text(this.icon, this.x, this.y);
+    this.y -= 3
+  }
+}
--- word2/sketch.js
+++ word2/sketch.js
@@ -1,13 +1,24 @@
-console.log('ready to sketch');
+const player = {
+  icon: '👰'
+};
+let beams = [];
+let beam;
+const zombies = ["🧟‍♀️","🧟","🧟‍♂️"];
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255, 140, 0);
-  textAlign(CENTER, CENTER);
   textSize(48);
   fill(238);
-  text('ready!', windowWidth/2, windowHeight/2);
+  text(player.icon, mouseX, mouseY);
+  for(beam of beams){
+    beam.shoot();
+  }
 }
 
+function mousePressed(){
+  beam = new Beam('🌷', mouseX, mouseY);
+  beams.push(beam);
+}
