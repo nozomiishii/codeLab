83f6c1601baa3edd247cd0b17310d85270fddb7b
Nozomi Ishii <non.ankt@gmail.com>
2019-12-15 15:57:13 +0900

    added p5 addons to the boilerplate

--- words/class.js
+++ words/class.js
@@ -1,1 +1,34 @@
 console.log('class.js');
+
+class Word {
+  constructor(text, x, y, shakeX, shakeY, fontSize){
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.shakeX = shakeX;
+    this.shakeY = shakeY;
+    this.fontSize = fontSize;
+  }
+  show(){
+    stroke(248);
+    fill(6);
+    textSize(this.fontSize);
+    text(this.text, this.x, this.y);
+  }
+  move(){
+    if(this.x + this.fontSize > width
+      || this.x - this.fontSize　< 0){
+      this.shakeX = -this.shakeX
+    }
+    if(this.y + this.fontSize > height
+      || this.y - this.fontSize　< 0){
+      this.shakeY = -this.shakeY
+    }
+
+
+    // this.x += random(-this.shakeX, this.shakeX);
+    // this.y += random(-this.shakeY, this.shakeY);
+    this.x += this.shakeX;
+    this.y += this.shakeY;
+  }
+}
--- words/sketch.js
+++ words/sketch.js
@@ -1,11 +1,23 @@
 console.log('ready to sketch');
+let word;
+let textbox;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // createCanvas(windowWidth, windowHeight);
+  // word = new Word(
+  //   'pussy',
+  //   random(width),
+  //   random(height),
+  //   random(15),
+  //   random(15),
+  //   30)
+  noCanvas();
+  textbox = createSlider(0, 100, 10);
 }
 
-function draw() {
-  background(220);
-  textAlign(CENTER, CENTER);
-  text('ready!', windowWidth/2, windowHeight/2);
-}
+// function draw() {
+//   background(6);
+//   textAlign(CENTER, CENTER);
+//   word.show();
+//   word.move();
+// }
 
