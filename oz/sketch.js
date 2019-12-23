c5039ef12649b9ddd1a040268cbf63a32b82085e
Nozomi Ishii <non.ankt@gmail.com>
2019-12-24 01:01:30 +0900

    save

--- oz/sketch.js
+++ oz/sketch.js
@@ -11,8 +11,10 @@ const animals = ['🐕','🐖','🦕','🐑','🦜','🦆','🐜','🦒','🦝',
 
 
 function acrossFriends(){
-  let randomIcon = animals[Math.floor(Math.random() * animals.length)];
-  friends.push(new Friend(randomIcon));
+  if(friends.length < 10){
+    let randomIcon = animals[Math.floor(Math.random() * animals.length)];
+    friends.push(new Friend(randomIcon));
+  }
 }
 
 function setup() {
@@ -26,15 +28,54 @@ function setup() {
 
 function draw() {
   background(6, 21, 30);
+  // oz show
   oz.show();
+
+  // trees show
   for(tree of trees){
     tree.show();
   }
-  for([index, friend] of friends.entries()){
+  // friend show
+  for(friend of friends){
     friend.show();
   }
-  // for(friend of friends){
-  //   friend.show();
-  // }
+
+  // judgement
+  for([index, friend] of friends.entries()){
+    let d = dist(friend.x, friend.y, mouseX, mouseY);
+    if(d < 42){
+      friends.splice(index, 1);
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
