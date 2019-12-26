99f0058d2fbab6d3e0da8b14aa4b800ee7a795ce
Nozomi Ishii <non.ankt@gmail.com>
2019-12-27 02:14:08 +0900

    giraffe

--- test/sketch.js
+++ test/sketch.js
@@ -1,19 +1,26 @@
 console.log('ready to sketch');
 
-const API_KEY = "bd296703bc29461ffafa94fd3b01fcd3";
-const BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+const api_key = "bd296703bc29461ffafa94fd3b01fcd3";
+const api = "http://api.openweathermap.org/data/2.5/weather";
 
-let url = BASE_URL + `?q=tokyo,jp&APPID=${API_KEY}&units=metric`;
+let url = api + `?q=Tokyo&APPID=${api_key}&units=metric`;
 
-let weather;
+let animal;
 
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+function preload(){
+  animal = loadJSON('animal.json');
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function gotData(data){
-  weather = data
+  weatherData = data
+  console.log(weatherData);
 }
 
 function setup() {
@@ -21,17 +28,33 @@ function setup() {
   loadJSON(url, gotData);
 }
 
+let giraffe;
+let giraffeX = width / 2;
+let giraffeY = height / 2;
+let show = false;
 
+let weatherData;
+let weather;
+let temperature;
 
 function draw() {
-  background(255, 140, 0);
+  background(50, 140, 20);
   textAlign(CENTER, CENTER);
-  textSize(48);
   fill(238);
-  if(weather){
-    console.log(weather.main.humidoty)
-    // text(weatherData.main, 100, 100);
-  }
+  textSize(22);
+  text("let's ask today's weather to him", width / 2, 100);
+  textSize(48);
+  giraffe = text(animal.giraffe, giraffeX, giraffeY);
+  giraffeX += random(-1,1);
+  giraffeY += random(-1,1);
 
+  if(show){
+    // text(weatherData.weather[0].main, 100, 100);
+    // text(weatherData.weather[0].main, 100, 100);
+  }
+}
+
+function mousePressed(){
+  show = true;
 }
 
