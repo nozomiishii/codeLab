f28378fa08a94793e89e45be439ce3afd84e5ee9
Nozomi Ishii <non.ankt@gmail.com>
2019-12-29 14:46:16 +0900

    done

--- wordnik/app.js
+++ wordnik/app.js
@@ -14,13 +14,21 @@ const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 const newYearCard = document.querySelector(".newYearCard");
 
+let clicked = false;
 newYearCard.addEventListener("click", getText);
 function getText() {
   fetch("text.txt")
     .then(response => response.text())
     .then(data => {
-      btn.innerHTML = "🐭";
-      output.innerHTML = data;
+      if (!clicked) {
+        btn.innerHTML = "🐭";
+        output.innerHTML = data;
+        return (clicked = true);
+      } else {
+        btn.innerHTML = "🐗";
+        output.innerHTML = "Thanks 2019";
+        return (clicked = false);
+      }
     })
     .catch(err => console.log(err));
 }
