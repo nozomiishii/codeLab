dd65896594124586ae4901d878bea12f71231454
Nozomi Ishii <non.ankt@gmail.com>
2019-12-30 19:21:09 +0900

    added p5js snippets

--- giphy/sketch.js
+++ giphy/sketch.js
@@ -1,11 +1,23 @@
 console.log('ready to sketch');
+
+const api = "https://api.giphy.com/v1/gifs/search?";
+const apiKey = '&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz';
+let query = '&q=mosnter';
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function gotData(giphy) {
+  createImg()
+  console.log(giphy.data[0].url);
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  url = api + query + apiKey;
+  loadJSON(url, gotData);
 }
 
 
@@ -14,8 +26,7 @@ function draw() {
   textAlign(CENTER, CENTER);
   textSize(48);
   fill(238);
-  text('ready!', windowWidth/2, windowHeight/2);
+  text('ready!', windowWidth / 2, windowHeight / 2);
   ellipseMode(CENTER);
-  ellipse(windowWidth/2, windowHeight/2 + 90, random(40), random(40));
+  ellipse(windowWidth / 2, windowHeight / 2 + 90, random(40), random(40));
 }
-
