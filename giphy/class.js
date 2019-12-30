eb8f04f4f309412a35b31b3ffcf536bfb498acd2
Nozomi Ishii <non.ankt@gmail.com>
2019-12-30 23:16:16 +0900

    test

--- giphy/class.js
+++ giphy/class.js
@@ -1,1 +1,14 @@
 console.log("class.js");
+
+class Image {
+  constructor(url) {
+    this.url = url;
+    this.x = mouseX;
+    this.y = mouseY;
+    this.width = random(10, 20);
+    this.height = random(10, 20);
+  }
+  show() {
+
+  }
+}
--- giphy/sketch.js
+++ giphy/sketch.js
@@ -1,32 +1,21 @@
-console.log('ready to sketch');
+console.log("hello")
 
-const api = "https://api.giphy.com/v1/gifs/search?";
-const apiKey = '&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz';
-let query = '&q=mosnter';
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+let img;
 
-function gotData(giphy) {
-  createImg()
-  console.log(giphy.data[0].url);
+function preload() {
+  img = loadImage('day070.png');
+  console.log(img);
+
 }
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  url = api + query + apiKey;
-  loadJSON(url, gotData);
+  noCanvas()
+  // createCanvas(1000, 1000);
+  image(img, 40, 40)
 }
 
-
 function draw() {
-  background(255, 140, 0);
-  textAlign(CENTER, CENTER);
-  textSize(48);
-  fill(238);
-  text('ready!', windowWidth / 2, windowHeight / 2);
-  ellipseMode(CENTER);
-  ellipse(windowWidth / 2, windowHeight / 2 + 90, random(40), random(40));
+  background(200);
 }
