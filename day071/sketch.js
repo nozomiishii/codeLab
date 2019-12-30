b8e9a21960173b2766ec448082a704c1e4e1d367
Nozomi Ishii <non.ankt@gmail.com>
2019-12-31 05:19:58 +0900

    debug

--- day071/sketch.js
+++ day071/sketch.js
@@ -16,7 +16,7 @@ let imgs = [];
 function draw() {
 	background(255);
 	textAlign(CENTER);
-	textSize(59);
+	textSize(48);
 	text('できた、、😭', windowWidth / 2, windowHeight / 2);
 	for (let i = 0; i < imgs.length; i++) {
 		image(img, imgs[i].x, imgs[i].y, imgs[i].w, imgs[i].h);
