d8bb2d9273014fe2d72952a7fff1804125f70e70
Nozomi Ishii <non.ankt@gmail.com>
2019-12-31 12:57:18 +0900

    new boilerplate

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,8 +1,15 @@
 console.log('ready to sketch');
+
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+let img;
+
+function preload() {
+  img = loadImage('kusayari.png');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,11 +18,13 @@ function setup() {
 
 function draw() {
   background(255, 140, 0);
+  imageMode(CENTER);
+  image(img, windowWidth * 0.5, windowHeight * 0.3, 300, 300);
+
   textAlign(CENTER, CENTER);
   textSize(48);
   fill(238);
-  text('ready!', windowWidth/2, windowHeight/2);
+  text('ready!', windowWidth * 0.5, windowHeight * 0.7);
   ellipseMode(CENTER);
-  ellipse(windowWidth/2, windowHeight/2 + 90, random(40), random(40));
+  ellipse(windowWidth * 0.5, windowHeight * 0.7 + 90, random(40), random(40));
 }
-
