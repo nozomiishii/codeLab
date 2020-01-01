43ceff6fad0a3b7ccd3e97c7cd8179dcc086f91c
Nozomi Ishii <non.ankt@gmail.com>
2020-01-02 02:58:57 +0900

    desire

--- desire/class.js
+++ desire/class.js
@@ -1,14 +1,25 @@
 // classes of desire 
-
+const desires = [];
 class Desire {
   constructor(img) {
     this.img = img;
     this.x = random(windowWidth);
     this.y = random(windowHeight);
-    this.sizeX = random(20, 40);
-    this.sizeY = random(20, 40);
+    this.sizeX = random(80, 120);
+    this.sizeY = random(80, 120);
   }
   show() {
-    image(this.img, this.x, this.y, this.sizeX * 2, this.sizeY * 2);
+    image(this.img, this.x, this.y, this.sizeX, this.sizeY);
+    this.x += random(-1, 1)
+    this.y += random(-1, 1)
+  }
+  clicked() {
+    let dx = Math.abs(this.x - mouseX);
+    let dy = Math.abs(this.y - mouseY);
+    if (dx < this.sizeX && dy < this.sizeY) {
+      darkness += 5;
+      heroSize -= 15;
+      desires.splice(this, 1);
+    }
   }
 }
--- desire/sketch.js
+++ desire/sketch.js
@@ -2,20 +2,26 @@ console.log('ready to sketch');
 // sketch of desire 
 const api = 'https://api.giphy.com/v1/gifs/search?';
 const apiKay = '&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz';
-let query = '&q=desire';
+let query = '&q=boobs';
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+// create new desire
 function getDesire(json) {
-  randomDesire = Math.floor(Math.random() * json.data.length);
-  createImg(json.data[randomDesire].images.original.url);
+  setInterval(() => {
+    randomDesire = Math.floor(Math.random() * json.data.length);
+    img = loadImage(json.data[randomDesire].images.original.url);
+    desires.push(new Desire(img));
+    // console.log(desires);
+  }, 1000);
 }
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // get images of desire from api 
   url = api + query + apiKay;
   fetch(url)
     .then(response => response.json())
@@ -27,13 +33,33 @@ let darkness = 6;
 
 
 let hero;
-let heroY = window.innerHeight / 2;
+let heroY = window.innerHeight / 2 + 100;
+let heroSize = 300;
 
 function draw() {
   background(darkness);
   textAlign(CENTER);
-  textSize(69);
-  hero = text('🦸🏻‍♂️', windowWidth / 2, heroY);
-  console.log(hero);
-  heroY += random(-1, 1);
+  textSize(heroSize);
+  if (heroSize > 0) {
+    heroY += random(-1, 1);
+    hero = text('🦸🏻‍♂️', windowWidth / 2, heroY);
+    for (let i = 0; i < desires.length; i++) {
+      desires[i].show();
+    }
+  } else {
+    background(255);
+    textSize(92);
+    text('👶🏻', windowWidth / 2, windowHeight * 0.4);
+    textSize(48);
+    fill(51, 153, 255);
+    text('清らかだなあ', windowWidth / 2, windowHeight * 0.6);
+  }
+
+}
+
+function mousePressed() {
+  for (let i = 0; i < desires.length; i++) {
+    desires[i].clicked();
+
+  }
 }
