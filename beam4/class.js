ad771bc1b49f6d6c0a52dc6e47c5a1dfcc077d9f
Nozomi Ishii <non.ankt@gmail.com>
2020-01-03 03:45:41 +0900

    created random monsters

--- beam4/class.js
+++ beam4/class.js
@@ -1,43 +1,77 @@
 console.log('class.js is here');
+// general settings 
+const ww = window.innerWidth;
+const wh = window.innerHeight;
 
-class Ball{
-  constructor(x, y, r, colorR, colorG, colorB, alpha, speedX, speedY, times){
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.colorR = colorR;
-    this.colorG = colorG;
-    this.colorB = colorB;
-    this.alpha = alpha;
-    this.speedX = speedX;
-    this.speedY = speedY;
-    this.times = times;
+// monsters class 
+const monsters = [];
+class Monster {
+  constructor(img) {
+    this.img = img;
+    this.x = Math.random() * ww;
+    this.y = Math.random() * wh;
+    this.speedX = 1;
+    this.speedY = 1;
+    this.size = 60;
   }
-  show(){
-    stroke(random(255),random(255),random(255));
-    fill(this.colorR, this.colorG, this.colorB, this.alpha);
-    ellipse(this.x, this.y, this.r * 2);
+  show() {
+    image(this.img, this.x, this.y, this.size, this.size);
   }
-  move(){
-    if(this.x > width || this.y > height || this.x < 0 || this.y < 0){
-      this.speedX = this.speedX * -1
-      this.speedY = this.speedY * -1
+  move() {
+    if (this.x > ww || this.y > wh || this.x < 0 || this.y < 0) {
+      this.speedX = -this.speedX;
+      this.speedY = -this.speedY;
     }
     this.x = this.x + this.speedX;
     this.y = this.y + this.speedY;
   }
-  // backgroundColor(){
-  //   background(this.colorR, this.colorG, this.colorB, this.alpha);
-  // }
+  hitBox(againstX, againstY) {
+    let dx = Math.abs(this.x - againstX);
+    let dy = Math.abs(this.y - againstY);
+
+    if (dx < this.x) {
+      this.x = -this.x;
+    }
+    if (dy < this.y) {
+      this.y = -this.y;
+    }
+    // let d = dist(this.x, this.y, againstX, againstY);
+    // if (d < 50) {
+
+    //   monster.speedX += random(-1, 1);
+    //   monster.speedY += random(-1, 1);
+    //   // monster.r++
+    //   // monster.times++
+    //   // totalScoreColorR = random(255);
+    //   // totalScoreColorG = random(255);
+    //   // totalScoreColorB = random(255);
+    //   // if (!dead) {
+    //   //   totalScore++
+    //   // }
+    // }
+    if (monsters.size < 0) {
+      // explode when monsters are attacked over 60 times
+      monsters.splice(i, 1);
+
+      // chenge the background color when beam hit monsters
+      setTimeout(function () {
+        if (backgroundColor <= 0 || backgroundColor >= 255) {
+          backgroundColorRate = backgroundColorRate * -1;
+        }
+        backgroundColor -= backgroundColorRate;
+      }, 1000);
+    }
+  }
 }
 
+const beams = [];
 class Beam {
-  constructor(x, y, speed){
+  constructor(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
   }
-  shoot(){
+  shoot() {
     stroke(6);
     fill(random(255));
     ellipse(this.x, this.y, 5, 20);
@@ -45,3 +79,15 @@ class Beam {
   }
 }
 
+const player = {
+  image: '👨🏻‍🚀',
+  x: ww / 2,
+  y: wh / 2,
+  life: 3,
+  show() {
+    this.x = mouseX;
+    this.y = mouseY;
+    textSize(46);
+    text(this.image, this.x, this.y);
+  }
+}
--- beam4/sketch.js
+++ beam4/sketch.js
@@ -1,10 +1,5 @@
 console.log('ready to sketch');
 
-let balls = [];
-let ball;
-let player;
-let beams = [];
-let beam;
 let backgroundColor = 236;
 let backgroundColorRate = 50;
 let totalScore = 0;
@@ -19,9 +14,7 @@ const end = document.querySelector('.end');
 const displayScore = document.querySelector('.end p');
 
 // let exploded = false;
-const api = 'https://api.giphy.com/v1/gifs/search?';
-const apiKey = '&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz';
-let query = "&q=monster"
+const monstersApi = 'https://api.giphy.com/v1/gifs/search?&q=monster&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz';
 
 
 
@@ -29,155 +22,65 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function searchGiphies(){
-  url = api + query + apiKey;
-  loadJSON(url, gotData);
-}
-
-let giphes = [];
-let imgCount = 0;
-function gotData(imgData){
-  setInterval(function(){
-    while(imgCount < imgData.data.length){
-      giphy = imgData.data[imgCount].images.original.url;
-      giphes.push(giphy);
-      imgCount++;
-      console.log(giphes);
-    }
-  }, 100);
-  
+function createMonsters() {
+  fetch(monstersApi)
+    .then(response => response.json())
+    .then(json => {
+      setInterval(() => {
+        random = Math.floor(Math.random() * json.data.length);
+        img = loadImage(json.data[random].images.original.url);
+        monsters.push(new Monster(img));
+        // console.log(monsters);
+      }, 100);
+    })
+    .catch(err => console.log(err));
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  creatingBalls = setInterval(function(){
-    ball = new Ball(
-    random(width),
-    random(height ),
-    random(20,46),
-    random(255), //colorR
-    random(255), //colorG
-    random(255), //colorB
-    random(255), //alpha
-    random(-1,1), //speedX
-    random(-1,1), //speedY
-    floor(random(9)) //times
-    );
-  balls.push(ball);
-  }, 5000);
-  searchGiphies();
+  createMonsters();
 }
 
-
-function mousePressed(){
-  beam = new Beam(
-    mouseX,
-    mouseY,
-    -3
-    );
-  beams.push(beam);
-}
-
-function draw(){
+function draw() {
   background(backgroundColor);
   textSize(24);
   stroke(6);
   fill(totalScoreColorR, totalScoreColorG, totalScoreColorB);
-  player = {
-    image: '👨🏻‍🚀',
-    x: mouseX,
-    y: mouseY,
-    life: 3,
-    show(){
-      textSize(46);
-      text(this.image, this.x, this.y);
-    }
-  }
-  // Score board
-  // text(`Score:${totalScore}`, 20, 50);
-  // for(let i = 0; i < playerLife; i++){
-  //   text('📡', 20 + i * 30, height - 50);
-  // }
 
   // controll player
   player.show();
-  for(ball of balls){
-    ball.show();
-    ball.move();
+  for (monster of monsters) {
+    monster.show();
+    monster.move();
   }
 
   // beams to balls
-  for(beam of beams){
+  for (beam of beams) {
     beam.shoot();
-    for([index, ball] of balls.entries()){
-      let d = dist(ball.x, ball.y, beam.x, beam.y );
-      if(d < 50){
-        ball.colorR = random(255);
-        ball.colorG = random(255);
-        ball.colorB = random(255);
-        ball.alpha = random(255);
-        ball.speedX += random(-1,1);
-        ball.speedY += random(-1,1);
-        ball.r++
-        ball.times++
-        totalScoreColorR = random(255);
-        totalScoreColorG = random(255);
-        totalScoreColorB = random(255);
-        if(!dead){
-          totalScore++
-        }
-      }
-      if(ball.times > 60){
-        // explode when balls are attacked over 60 times
-        balls.splice(index, 1);
+    for (let i = 0; i < monsters.length; i++) {
+      monsters[i].hitBox(beam.x, beam.y);
+    }
 
-        // chenge the background color when beam hit balls
-        setTimeout(function(){
-          if(backgroundColor <= 0 || backgroundColor >= 255){
-            backgroundColorRate = backgroundColorRate * -1;
-          }
-          backgroundColor -= backgroundColorRate;
-        }, 1000);
-      }
-    }
-  }
-
-  // balls to player
-  for(ball of balls){
-    let d = dist(ball.x, ball.y, mouseX, mouseY)
-    if(d < ball.r){
-      playerLife--
-      console.log(playerLife);
-    }
-    if(playerLife < 0){
-      end.classList.add('ended');
-      displayScore.innerHTML = `Your Score: ${totalScore}`;
-      return dead = true;
-    }
+    // monsters to player
+    // for (monster of monsters) {
+    //   let d = dist(monster.x, monster.y, mouseX, mouseY)
+    //   if (d < monster.r) {
+    //     playerLife--
+    //     console.log(playerLife);
+    //   }
+    //   if (playerLife < 0) {
+    //     end.classList.add('ended');
+    //     displayScore.innerHTML = `Your Score: ${totalScore}`;
+    //     return dead = true;
+    //   }
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function mousePressed() {
+  beam = new Beam(
+    mouseX,
+    mouseY,
+    -3
+  );
+  beams.push(beam);
+}
