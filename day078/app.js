4b5301cd74bb8ddf0f6cd9052730a931292b578e
Nozomi Ishii <non.ankt@gmail.com>
2020-01-04 21:41:59 +0900

    day78

---
+++ day078/app.js
@@ -0,0 +1,35 @@
+//check
+console.log('firebase');
+
+const start = document.getElementById('start');
+const op = document.querySelector('.op');
+
+start.addEventListener('click', function () {
+  op.classList.add('start');
+});
+
+
+db.collection('words').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+    renderWords(change.doc);
+  });
+});
+
+const outputBox = document.querySelector('#outputBox');
+
+function renderWords(doc) {
+  let wordlist = document.createElement('span');
+  wordlist.innerHTML = doc.data().word;
+  outputBox.appendChild(wordlist);
+}
+
+const form = document.querySelector('#input-word');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  db.collection('words').add({
+    word: `❤️❤️${form.word.value}❤️❤️`
+  });
+  form.word.value = "";
+})
