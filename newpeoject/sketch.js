d8528b88b24941710de59704f812f5739f622c61
Nozomi Ishii <non.ankt@gmail.com>
2020-01-09 02:32:13 +0900

    signiture

--- newpeoject/sketch.js
+++ newpeoject/sketch.js
@@ -5,11 +5,6 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-let img;
-
-function preload() {
-  img = loadImage('kusayari.png');
-}
 
 const start = document.getElementById('start');
 const op = document.querySelector('.op');
@@ -19,22 +14,13 @@ function setup() {
   //game starts
 
 
-  start.addEventListener('click', function () {
-    op.classList.add('start');
-  });
+  // start.addEventListener('click', function () {
+  //   op.classList.add('start');
+  // });
 
 }
 
 
 function draw() {
   background(255, 140, 0);
-  imageMode(CENTER);
-  image(img, windowWidth * 0.5, windowHeight * 0.3, 300, 300);
-
-  textAlign(CENTER, CENTER);
-  textSize(48);
-  fill(238);
-  text('ready!', windowWidth * 0.5, windowHeight * 0.7);
-  ellipseMode(CENTER);
-  ellipse(windowWidth * 0.5, windowHeight * 0.7 + 90, random(40), random(40));
 }
