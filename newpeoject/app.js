4c8aa10792fe6993e0b60e910e2ac3f22bec6174
Nozomi Ishii <non.ankt@gmail.com>
2020-01-08 21:28:45 +0900

    day81

---
+++ newpeoject/app.js
@@ -0,0 +1,9 @@
+//check
+console.log('app.js is here');
+
+// write game code from here
+window.addEventListener('touchmove', function (event) {
+  event.preventDefault();
+}, {
+  passive: false
+});
