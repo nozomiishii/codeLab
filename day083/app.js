cf6f954781d279cdb0e80076253feae05ab115e0
Nozomi Ishii <non.ankt@gmail.com>
2020-01-10 19:55:14 +0900

    debug

--- day083/app.js
+++ day083/app.js
@@ -2,11 +2,11 @@
 console.log('app.js is here');
 
 // write game code from here
-window.addEventListener('touchmove', function (event) {
-  event.preventDefault();
-}, {
-  passive: false
-});
+// window.addEventListener('touchmove', function (event) {
+//   event.preventDefault();
+// }, {
+//   passive: false
+// });
 
 
 
