22d863171f1eeb97a9906f650160e54bb7f98887
Nozomi Ishii <non.ankt@gmail.com>
2020-01-15 01:53:18 +0900

    test

--- camera/sketch.js
+++ camera/sketch.js
@@ -1,63 +1,18 @@
-console.log('ready to sketch');
-
-// const constraints = {
-//   audio: false,
-//   video: {
-//     width: window.innerWidth,
-//     height: window.innerHeight * 0.6
-//   }
-// };
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-let capture;
-let canvas;
-let w;
-let h;
-
-
-// navigator.mediaDevices.getUserMedia(constraints).then(
-//   console.log('work')
-// )
-
 function setup() {
-  w = windowWidth;
-  h = windowHeight * 0.6;
-
-  // const shotBtn = document.getElementById('shotBtn');
-  canvas = createCanvas(w, h);
-
-  capture = createCapture({
-    audio: false,
+  createCanvas(480, 120);
+  let constraints = {
     video: {
-      width: w,
-      height: h
-    }
-  }, function () {
-    console.log('capture ready.')
+      mandatory: {
+        minWidth: 1280,
+        minHeight: 720
+      },
+      optional: [{
+        maxFrameRate: 10
+      }]
+    },
+    audio: true
+  };
+  createCapture(constraints, function (stream) {
+    console.log(stream);
   });
-  capture.elt.setAttribute('playsinline', '');
-  capture.hide();
-  capture.size(w, h);
-
-
-
-
-  // shotBtn.addEventListener('click', () => {
-  //   console.log('click');
-  //   capture.loadPixels();
-  //   const image64 = capture.canvas.toDataURL();
-  //   console.log(image64);
-  //   const image = document.createElement('img');
-  //   image.src = image64;
-  //   image.alt = 'photo'
-  //   document.body.append(image);
-  // });
-}
-
-
-function draw() {
-  image(capture, 0, 0);
 }
