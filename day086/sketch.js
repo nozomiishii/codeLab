4b39da8abaf412f3ef9b1e72ca2e1a0416de7b85
Nozomi Ishii <non.ankt@gmail.com>
2020-01-15 00:35:56 +0900

    debug

--- day086/sketch.js
+++ day086/sketch.js
@@ -5,26 +5,48 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+let capture;
+let w;
+let h;
+
+
 
 function setup() {
+
   const shotBtn = document.getElementById('shotBtn');
   // createCanvas(windowWidth, windowHeight);
-  noCanvas();
-  //game starts
+  w = windowWidth;
+  h = windowHeight * 0.6;
+  capture = createCapture({
+    audio: false,
+    video: {
+      width: w,
+      height: h
+    }
+  }, function () {
+    console.log('capture ready.')
+  });
+  capture.elt.setAttribute('playsinline', '');
+  capture.hide();
+  capture.size(w, h);
+  canvas = createCanvas(w, h);
 
-  const video = createCapture(VIDEO);
-  video.size(windowWidth, windowHeight * 0.6);
-  video.elt.setAttribute('playsinline', '');
+
 
 
   shotBtn.addEventListener('click', () => {
     console.log('click');
-    video.loadPixels();
-    const image64 = video.canvas.toDataURL();
+    capture.loadPixels();
+    const image64 = capture.canvas.toDataURL();
     console.log(image64);
     const image = document.createElement('img');
     image.src = image64;
     image.alt = 'photo'
     document.body.append(image);
   });
+}
+
+
+function draw() {
+  image(capture, 0, 0);
 }
