6fb82ec150dcff951a06e53b40db61989f29b48e
Nozomi Ishii <non.ankt@gmail.com>
2020-01-17 05:09:24 +0900

    tesst

--- photo/sketch.js
+++ photo/sketch.js
@@ -6,68 +6,68 @@ function windowResized() {
 
 
 
-const capture = document.getElementById('capture');
-async function displayCapture() {
-  const media = await navigator.mediaDevices.getUserMedia(constraints);
-  capture.srcObject = media;
-  console.log(media);
-}
+// const capture = document.getElementById('capture');
+// async function displayCapture() {
+//   const media = await navigator.mediaDevices.getUserMedia(constraints);
+//   capture.srcObject = media;
+//   console.log(media);
+// }
 
 
-let front = false;
-const constraints = {
-  audio: false,
-  video: {
-    facingMode: (front ? "user" : "environment")
-  }
-};
-const swichCameraBtn = document.getElementById('swichCameraBtn');
-swichCameraBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log(11);
-  front = !front;
-  document.body.style.background = '#45D8EA'
-  displayCapture().catch(err => console.error(err));
-});
+// let front = false;
+// const constraints = {
+//   audio: false,
+//   video: {
+//     facingMode: (front ? "user" : "environment")
+//   }
+// };
+// const swichCameraBtn = document.getElementById('swichCameraBtn');
+// swichCameraBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log(11);
+//   front = !front;
+//   document.body.style.background = '#45D8EA'
+//   displayCapture().catch(err => console.error(err));
+// });
 
 
 // displayCapture().catch(err => console.error(err));
 
 
 // plan B 
-// const swichCameraBtn = document.getElementById('swichCameraBtn');
-// let cameraFacing = false;
+const swichCameraBtn = document.getElementById('swichCameraBtn');
+let cameraFacing = false;
 
-// // switch camera 
-// swichCameraBtn.addEventListener("click", function (e) {
+// switch camera 
+swichCameraBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.body.style.background = '#BD3023'
 
-//   e.preventDefault();
-
-//   const capture = document.getElementById('capture');
-//   const mode = cameraFacing ? "environment" : "user";
-
-
-
-//   // カメラ切り替え
-//   navigator.mediaDevices.getUserMedia({
-//       video: {
-//         facingMode: mode
-//       }
-//     })
-//     .then(stream => capture.srcObject = stream)
-//     .catch(err => alert(`${err.name} ${err.message}`));
-
-//   cameraFacing = !cameraFacing;
-// });
+  const capture = document.getElementById('capture');
+  const mode = cameraFacing ? "environment" : "user";
 
 
-// function setup() {
-//   noCanvas();
-//   // createCanvas(windowWidth, windowHeight);
-//   //game starts
-// }
+
+  // カメラ切り替え
+  navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: mode
+      }
+    })
+    .then(stream => capture.srcObject = stream)
+    .catch(err => alert(`${err.name} ${err.message}`));
+
+  cameraFacing = !cameraFacing;
+});
 
 
-// function draw() {
-//   background(255, 140, 0);
-// }
+function setup() {
+  noCanvas();
+  // createCanvas(windowWidth, windowHeight);
+  //game starts
+}
+
+
+function draw() {
+  background(255, 140, 0);
+}
