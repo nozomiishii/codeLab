8bd5579683ea615da4118467387289fe72e2e6c6
Nozomi Ishii <non.ankt@gmail.com>
2020-01-17 20:08:42 +0900

    capture

---
+++ capture/sketch.js
@@ -0,0 +1,36 @@
+console.log('ready to sketch');
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+let cameraFacing = false;
+const capture = document.getElementById('capture');
+const mode = cameraFacing ? "environment" : "user";
+
+navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: mode
+    }
+  })
+  .then(stream => capture.srcObject = stream)
+  .catch(err => console.error(err));
+
+const cameraFacingBtn = document.getElementById('cameraFacingBtn');
+
+cameraFacingBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  cameraFacing = !cameraFacing;
+  console.log(cameraFacing);
+})
+
+
+function setup() {
+  noCanvas();
+  //game starts
+}
+
+
+// function draw() {
+//   background(255, 140, 0);
+// }
