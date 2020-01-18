29f8bb3a1ec52aaf65e53a63fe7fa8ad4e2a65eb
Nozomi Ishii <non.ankt@gmail.com>
2020-01-18 20:16:48 +0900

    ink

---
+++ ink/sketch.js
@@ -0,0 +1,50 @@
+console.log('ready to sketch');
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+const start = document.getElementById('start');
+const op = document.querySelector('.op');
+start.addEventListener('click', function () {
+  op.classList.add('start');
+});
+
+let ww, wh;
+
+function setup() {
+  ww = windowWidth;
+  wh = windowHeight;
+  createCanvas(ww, wh);
+  background(238);
+  //game starts
+  capture = createCapture({
+    audio: false,
+    video: {
+      width: ww,
+      height: wh
+    }
+  }, function () {
+    console.log('capture ready.')
+  });
+  capture.elt.setAttribute('playsinline', '');
+  // capture.hide();
+
+  const photoShot = document.getElementById('photoShot');
+  photoShot.addEventListener("click", () => {
+    capture.loadPixels();
+    photos.push(new Photo(capture));
+  });
+}
+
+
+function draw() {
+  // image(capture, 0, 0, ww, wh);
+  clear();
+  if (photos.length > 0) {
+    for (let i = 0; i < photos.length; i++) {
+      photos[i].show();
+      photos[i].move();
+    }
+  }
+}
