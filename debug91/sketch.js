80130434b06608595588139728f16129cbab89f1
Nozomi Ishii <non.ankt@gmail.com>
2020-01-18 17:04:06 +0900

    test

---
+++ debug91/sketch.js
@@ -0,0 +1,53 @@
+console.log('ready to sketch');
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// const start = document.getElementById('start');
+// const op = document.querySelector('.op');
+// start.addEventListener('click', function () {
+//   op.classList.add('start');
+// });
+
+
+
+
+
+function setup() {
+  ww = windowWidth;
+  wh = windowHeight;
+  createCanvas(ww, wh);
+  background(6);
+  //game starts
+  capture = createCapture({
+    audio: false,
+    video: {
+      width: ww,
+      height: wh
+    }
+  }, function () {
+    console.log('capture ready.')
+  });
+  capture.elt.setAttribute('playsinline', '');
+  capture.hide();
+  capture.size(ww, wh);
+
+  const photoShot = document.getElementById('photoShot');
+  photoShot.addEventListener("click", () => {
+    img = capture.loadPixels();
+
+    photos.push(new Photo(img));
+  });
+}
+
+
+function draw() {
+  image(capture, 0, 0);
+  if (photos.length > 0) {
+    for (let i = 0; i < photos.length; i++) {
+      photos[i].show();
+      photos[i].move();
+    }
+  }
+}
