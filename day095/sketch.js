ba7fc353e1c6562d31a4f1b1e21f54f6b9edb04c
Nozomi Ishii <non.ankt@gmail.com>
2020-01-23 02:23:57 +0900

    vshero

--- day095/sketch.js
+++ day095/sketch.js
@@ -1,26 +1,60 @@
 console.log('ready to sketch');
+const url = "https://api.giphy.com/v1/gifs/search?&q=hero&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz"
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+let imgs = [];
+let img;
+
+function gotData(giphy) {
+  for (let i = 0; i < 10; i++) {
+    img = loadImage(giphy.data[i].images.original.url);
+    imgs.push(img);
+  }
+}
+
+function preload() {
+  loadJSON(url, gotData);
+}
+
+let hero, villain, heroImg, villainImg, randomHero, randomVillain;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   //game starts
   noStroke()
   textAlign(CENTER);
+  imageMode(CENTER);
 }
-
 
 function draw() {
   background(230);
 
-  textSize(mouseY)
-  text('🦸🏻‍♀️', mouseX, height * .6);
+  if (randomHero) {
+    image(imgs[randomHero], mouseX, height * .6, mouseY, mouseY);
+  } else {
+    textSize(mouseY)
+    hero = '🦸🏻‍♀️';
+    text(hero, mouseX, height * .6);
+  }
 
   let inverseX = width - mouseX;
   let inverseY = height - mouseY;
 
-  textSize(inverseY)
-  text('🦹🏻‍♀️', inverseX, height * .6);
+  if (randomVillain) {
+    image(imgs[randomVillain], inverseX, height * .6, inverseY, inverseY);
+  } else {
+    textSize(inverseY)
+    villain = '🦹🏻‍♀️'
+    text(villain, inverseX, height * .6);
+  }
+}
+
+function mousePressed() {
+
+  randomHero = Math.floor(Math.random() * imgs.length);
+
+  randomVillain = Math.floor(Math.random() * imgs.length);
 }
