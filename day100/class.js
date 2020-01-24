f4c2818cece6380457a8e103e99556b305314787
Nozomi Ishii <non.ankt@gmail.com>
2020-01-24 18:43:25 +0900

    day100

---
+++ day100/class.js
@@ -0,0 +1,80 @@
+console.log('class.js is here');
+// general settings 
+const ww = window.innerWidth;
+const wh = window.innerHeight;
+
+// monsters class 
+const monsters = [];
+class Monster {
+  constructor(img) {
+    this.img = img;
+    this.x = Math.random() * ww;
+    this.y = Math.random() * wh;
+    this.speedX = random(-1, 1);
+    this.speedY = random(-1, 1);
+    this.size = 60;
+  }
+  show() {
+    image(this.img, this.x, this.y, this.size, this.size);
+  }
+  move() {
+    if (this.x > ww || this.y > wh || this.x < 0 || this.y < 0) {
+      this.speedX = -this.speedX;
+      this.speedY = -this.speedY;
+    }
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+  }
+  hitBox(againstX, againstY) {
+
+    // monster against beams 
+    let dx = Math.abs(this.x - againstX);
+    let dy = Math.abs(this.y - againstY);
+    if (dx < this.size && dy < this.size) {
+      if (this.size < 30) {
+
+        this.x = 0;
+        this.y = 0;
+      } else {
+        this.size--;
+        this.speedX += random(-1, 1);
+        this.speedY += random(-1, 1);
+        //   // chenge the background color when beam hit monsters
+        if (!dead) {
+          totalScore++
+
+        }
+      }
+    }
+  }
+}
+
+const beams = [];
+class Beam {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = -3;
+  }
+  shoot() {
+    stroke(6);
+    fill(random(255));
+    ellipse(this.x, this.y, 5, 20);
+    this.y += this.speed;
+  }
+}
+
+let player;
+class Player {
+  constructor(image) {
+    this.image = image;
+    this.x = ww / 2;
+    this.y = wh / 2;
+    this.size = 46;
+    this.life = 3;
+  }
+  show(x, y) {
+
+    image(this.image, x, y - this.size, this.size, this.size);
+  }
+}
---
+++ day100/sketch.js
@@ -0,0 +1,136 @@
+console.log('ready to sketch');
+
+//game starts
+const start = document.getElementById('start');
+const op = document.querySelector('.op');
+
+
+let backgroundColor = 6;
+let backgroundColorRate = 50;
+let totalScore = 0;
+let totalScoreColorR = 238;
+let totalScoreColorG = 238;
+let totalScoreColorB = 238;
+let dead = false;
+let creatingBalls;
+let playerLife = 3;
+let capture;
+
+const end = document.querySelector('.end');
+const displayScore = document.querySelector('.end p');
+
+// let exploded = false;
+const monstersApi = 'https://api.giphy.com/v1/gifs/search?&q=monster&api_key=2yzS5m3m1CWprh70TzZdDVjCqXRS3Qrz';
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+let monsterInterval;
+
+function createMonsters() {
+  fetch(monstersApi)
+    .then(response => response.json())
+    // .then(json => {
+    //   monsterInterval = setInterval(() => {
+    //     randomImg = Math.floor(Math.random() * json.data.length);
+    //     img = loadImage(json.data[randomImg].images.original.url);
+    //     console.log(img);
+    //     console.log(json.data[randomImg]);
+    //     monsters.push(new Monster(img));
+    //     // console.log(monsters);
+    //   }, 1000);
+    // })
+    .catch(err => console.log(err));
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // createMonsters();
+
+  let constrains = {
+    video: {
+      width: 46,
+      height: 46
+    }
+  }
+  capture = createCapture(constrains, () => console.log('capture ready.'));
+  capture.elt.setAttribute('playsinline', '');
+  capture.hide();
+
+  player = new Player(capture);
+
+
+  capture2 = createCapture(constrains, () => console.log('capture ready.'));
+  capture2.elt.setAttribute('playsinline', '');
+  capture2.hide();
+  monsterInterval = setInterval(() => {
+    capture2.loadPixels();
+    const image64 = capture2.canvas.toDataURL();
+    img = loadImage(image64);
+    // capture.updatePixels();
+    monsters.push(new Monster(img));
+    // console.log(monsters);
+  }, 2000);
+}
+
+
+
+function draw() {
+  background(backgroundColor);
+  textSize(24);
+  stroke(6);
+  fill(totalScoreColorR, totalScoreColorG, totalScoreColorB);
+
+  text(`Score:${totalScore}`, 20, 50);
+
+  // controll player
+
+  player.show(mouseX, mouseY);
+
+  for (monster of monsters) {
+    monster.show();
+    monster.move();
+  }
+
+  // beams to balls
+
+  for (let i = 0; i < beams.length; i++) {
+    beams[i].shoot(random(255));
+    for (let k = 0; k < monsters.length; k++) {
+      monsters[k].hitBox(beams[i].x, beams[i].y);
+    }
+  }
+
+  // monsters to player
+  for (monster of monsters) {
+    let dx = Math.abs(monster.x - mouseX);
+    let dy = Math.abs(monster.y - mouseY);
+    if (dx < monster.size && dy < monster.size) {
+      clearInterval(monsterInterval);
+      background(238);
+      return dead = true;
+    }
+  }
+
+  if (dead) {
+    background(238);
+    textAlign(CENTER);
+    textSize(16);
+    text('DEAD', width / 2, height * .4)
+    imageMode(CENTER);
+    player.show(width / 2, height * .5)
+    text(`Score:${totalScore}`, width / 2, height * .6);
+
+  }
+}
+
+function mousePressed() {
+  beam = new Beam(
+    mouseX,
+    mouseY,
+  );
+  beams.push(beam);
+}
