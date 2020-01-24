ebc75ecac0bd81d478500ba0e786053c2dc44344
Nozomi Ishii <non.ankt@gmail.com>
2020-01-24 23:47:23 +0900

    creating maps, but it fails

--- pokemon/class.js
+++ pokemon/class.js
@@ -1,1 +1,6 @@
-console.log("class.js");
+const monsters = [...'🦄🐿🦩🐊🦕🐝'];
+
+let monsterInterval = setInterval(() => {
+  let randomMonster = Math.floor(Math.random() * monsters.length);
+  // console.log(monsters[randomMonster]);
+}, 1000);
--- pokemon/sketch.js
+++ pokemon/sketch.js
@@ -4,12 +4,40 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+const icon = L.icon({
+  iconUrl: 'player.png',
+
+  iconSize: [32, 32], // アイコンのサイズ
+  iconAnchor: [16, 52], // マーカーの位置に対応するアイコンの位置
+  popupAnchor: [0, -55] // ポップアップを開く基準
+});
+
+function geolocation(lat, lng) {
+  map = L.map('map').setView([lat, lng], 16);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+      attribution: 'Map tiles by <a href="http://stamen.com" rel="nofollow noopener" target="_blank">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0" rel="nofollow noopener" target="_blank">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org" rel="nofollow noopener" target="_blank">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0" rel="nofollow noopener" target="_blank">CC BY SA</a>.',
+      maxZoom: 20
+    }
+  ).addTo(map);
+
+  L.marker([lat, lng], {
+      icon: icon
+    }).addTo(map)
+    .bindPopup('さあ、冒険だ')
+    .openPopup();
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   //game starts
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+
+      geolocation(lat, lng);
     });
     console.log('yay');
   } else {
@@ -18,13 +46,10 @@ function setup() {
 
 }
 
-
-function draw() {
-  background(48, 140, 9);
-
-  textAlign(CENTER, CENTER);
-  textSize(48);
-  fill(238);
-  text('👩🏻‍🎓: "ready!"', windowWidth * 0.5, windowHeight * 0.5);
-
+function drow() {
+  L.marker([mouseX, mouseX], {
+      icon: icon
+    }).addTo(map)
+    .bindPopup('さあ、冒険だ')
+    .openPopup();
 }
