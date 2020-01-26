b4cc216b29f4803ebe66afb00673264fd4754fb6
Nozomi Ishii <non.ankt@gmail.com>
2020-01-26 14:04:30 +0900

    connected to db

--- day097/app.js
+++ day097/app.js
@@ -1,14 +1,45 @@
 const scoreList = document.querySelector('#score-list');
+const rankList = document.querySelector('#rank-list');
 
-function renderData(doc) {
+// getting data 
+function scoreListRenderData(doc) {
   let li = document.createElement('li');
   li.setAttribute('data-id', doc.id);
-  li.innerHTML = ` <p>${doc.data().name}  ${doc.data().score}pt</p>`
+  li.innerHTML = `
+  <p>${doc.data().name}</p>
+  <p><span class="list-score">${doc.data().score}<span>pt</p>
+  `
   scoreList.appendChild(li);
+  li.classList.add('list-design');
 }
 
-db.collection('ranks').get().then(snapshot => {
+// result data 
+function rankListRenderData(doc) {
+  let li = document.createElement('li');
+  li.setAttribute('data-id', doc.id);
+  li.innerHTML = `
+  <p>${doc.data().name}</p>
+  <p><span class="list-score">${doc.data().score}<span>pt</p>
+  `
+  rankList.appendChild(li);
+  li.classList.add('list-design');
+}
+
+db.collection('ranks').orderBy("score", "desc").get().then(snapshot => {
   snapshot.docs.forEach(doc => {
-    renderData(doc);
+    scoreListRenderData(doc);
+    rankListRenderData(doc);
   });
+})
+
+// saving data 
+const form = document.querySelector("#rank-player");
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  await db.collection('ranks').add({
+    name: form.name.value,
+    score: totalScore
+  })
+  console.log("send");
+  location.reload();
 })
